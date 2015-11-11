734507ab5870e1f8f81f0444e63b4031729d66f5
Scott Skoot Miller <smmiller.work@gmail.com>
2015-11-10 18:55:20 -0700

    now done thru step 7

--- js/MainController.js
+++ js/MainController.js
@@ -1,9 +1,16 @@
 angular.module('sounder')
 
-   .controller('MainController', function($scope, soundService) {
+   .controller('MainController', function($scope, $sce, soundService) {
       $scope.getUser = function() {
          soundService.getUser($scope.searchText).then(function(response) {
-            console.log(response.data)
+            console.log(response.data);
+            $scope.userData = response.data;
+         });
+      };
+
+      $scope.play = function(track_url) {
+         SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+            $scope.$apply($scope.player_html = $sce.trustAsHtml(oEmbed.html));
          });
       };
 
